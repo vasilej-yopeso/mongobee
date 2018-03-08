cc28b7a703b2cd6cc908f7789006a41b9b83e437
Vasile Jureschi <vasile.jureschi@yopeso.com>
2018-03-08 15:40:22 +0200

    Added release 17

---
+++ install_jar.sh
@@ -0,0 +1,1 @@
+mvn install:install-file -DgroupId=com.github.mongobee -DartifactId=mongobee -Dversion=17 -Dfile=./target/mongobee-17.jar -Dpackaging=jar  -DlocalRepositoryPath=./repository  -DcreateChecksum=true
